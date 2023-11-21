bb6a46f21dcc6a2f06678113e80bb2deabd56baf
Gceccoli <guiceccoli@gmail.com>
2023-11-21 23:46:43 +0100

    Refactorisation du code + modification fonction de conversion + vérification supplémentaire

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
+function displayError() {
+  if (!document.getElementById("errorInfos")) {
+    let subContainer = document.getElementById("sub-container");
+    let errorInfos = document.createElement("div");
+    errorInfos.id = "errorInfos";
+    errorInfos.innerHTML = "Les informations sont erronées";
+    errorInfos.classList.add("error");
+    subContainer.appendChild(errorInfos);
+  }
+}
+
+function removeError() {
+  let errorElement = document.getElementById("errorInfos");
+  if (errorElement) {
+    errorElement.remove();
+  }
+}
+
+function calculateIMC(weight, size) {
+  let imc = weight / Math.pow(size / 100, 2);
+  imc = imc.toFixed(2);
+  localStorage.setItem("imc", imc);
+  window.location.href = "resultat.html?imc=" + imc;
+}
+
+function controlUnit() {
+  let unit = document.getElementById("unit").value;
+  if (unit !== "m" && unit !== "cm") {
+    alert("Unité de mesure invalide");
+    return false;
+  } else {
+    return true;
+  }
+}
+
 function userData() {
   const button = document.getElementById("sub");
   button.addEventListener("click", () => {
+    if (!controlUnit()) {
+      return;
+    }
     let weight = document.getElementById("weight").value;
     let size = document.getElementById("size").value;
-    if (size < 10) {
-      size *= 100;
+    let unit = document.getElementById("unit").value;
+    if (unit === "m") {
+      size = Number(size) * 100;
     }
     if (weight === "" || size === "") {
-      if (!document.getElementById("errorInfos")) {
-        // Vérifie si le message d'erreur existe déjà
-        let subContainer = document.getElementById("sub-container");
-        let errorInfos = document.createElement("div");
-        errorInfos.id = "errorInfos"; // Ajoute un identifiant à l'élément d'erreur
-        errorInfos.innerHTML = "Les informations sont erronées";
-        errorInfos.classList.add("error"); // Ajoute une classe à l'élément d'erreur
-        subContainer.appendChild(errorInfos);
-      }
+      displayError();
     } else {
-      // Si l'élément d'erreur existe, le supprimer avant de calculer l'IMC
-      let errorElement = document.getElementById("errorInfos");
-      if (errorElement) {
-        errorElement.remove();
-      }
-
-      let imc = weight / Math.pow(size / 100, 2);
-      imc = imc.toFixed(2);
-      localStorage.setItem("imc", imc);
-      window.location.href = "resultat.html?imc=" + imc;
+      removeError();
+      calculateIMC(weight, size);
     }
+    controlUnit();
   });
 }
 userData();
